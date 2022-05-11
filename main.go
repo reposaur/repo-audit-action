a46e5dbd3dfae2e8e1e73d777f8a62d8af017718
João Cerqueira <joao@cerqueira.io>
2022-05-11 14:41:13 +0100

    fix: log default policy path

--- main.go
+++ main.go
@@ -50,6 +50,7 @@ func (a *RepoAuditAction) Run() error {
 	)
 
 	if len(policyPaths) == 0 {
+		logger.Info().Msgf("No policy paths specified. Using '%s' as default", action.Context.Workspace)
 		policyPaths = append(policyPaths, action.Context.Workspace)
 	}
 
